2551f4cbe8758e770db0205cceb0983c8a48a70d
Boru Isako <isco.jaldy@gmail.com>
2025-06-02 17:14:29 +0300

    Refactor connection.py: move database connection setup outside of get_connection function

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,7 +1,8 @@
 # Database connection setup
 import sqlite3
 
+conn = sqlite3.connect('articles.db')
+conn.row_factory = sqlite3.Row 
+
 def get_connection():
-    conn = sqlite3.connect('articles.db')
-    conn.row_factory = sqlite3.Row 
     return conn
